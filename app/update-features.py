dc28ccc1c544eb27d1c004aa9ea5f1951da8bedd
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-08-02 20:34:27 -0500

    eza

--- app/update-features.py
+++ app/update-features.py
@@ -1,9 +1,9 @@
-import os, json
+import os, json, shutil
 import yaml
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
-MANAGED_FEATURES_DIR = os.path.join(SCRIPT_DIR, 'managed-features')
-FEATURES_DIR = os.path.join(SCRIPT_DIR, 'features')
+MANAGED_FEATURES_DIR = os.path.join(SCRIPT_DIR, '..', 'managed-features')
+FEATURES_DIR = os.path.join(SCRIPT_DIR, '..', 'features')
 
 
 def load_managed_features():
@@ -18,14 +18,35 @@ def load_managed_features():
 
 
 def create_feature(feature_info):
-    with open(os.path.join(FEATURES_DIR, feature_info['name'] + '.json'), 'w') as f:
-        f.write("\n")
+    devcontainer_json = {}
+    if 'brew' in feature_info.keys():
+        package_name = feature_info['brew']['package']
+        cmd = f"brew info --json {package_name}"
+        result = os.popen(cmd).read()
+        package_info = json.loads(result)[0]
+        devcontainer_json['name'] = feature_info['name']
+        devcontainer_json['id'] = feature_info['name']
+        devcontainer_json['description'] = package_info['desc']
+        devcontainer_json['documentationURL'] = package_info['homepage']
+        devcontainer_json['version'] = "0.0.1"
+        devcontainer_json['options'] = {
+            "version": {
+                "type": "string",
+                "description": f"Version of {feature_info['name']} to install",
+                "default": package_info['versions']['stable'],
+            }
+        }
+    if devcontainer_json:
+        os.makedirs(os.path.join(FEATURES_DIR,  feature_info['name']), exist_ok=True)
+        with open(os.path.join(FEATURES_DIR,  feature_info['name'],  'devcontainer-feature.json'), 'w') as f:
+            json.dump(devcontainer_json, f, indent=4)
 
 
 def empty_feature_dir():
-    for filename in os.listdir(FEATURES_DIR):
-        if filename.endswith('.json'):
-            os.remove(os.path.join(FEATURES_DIR, filename))
+    # Recursively delete all directories in FEATURES_DIR:
+    for root, dirs, files in os.walk(FEATURES_DIR, topdown=False):
+        for name in dirs:
+            shutil.rmtree(os.path.join(root, name))
 
 
 def main():
