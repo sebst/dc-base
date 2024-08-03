8d1f99c9ab4560adc60b5b849c3010458c680922
Sebastian Steins <592313+sebst@users.noreply.github.com>
2024-08-02 20:17:22 -0500

    Features...

--- app/update-features.py
+++ app/update-features.py
@@ -5,13 +5,35 @@ SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 MANAGED_FEATURES_DIR = os.path.join(SCRIPT_DIR, 'managed-features')
 FEATURES_DIR = os.path.join(SCRIPT_DIR, 'features')
 
+
 def load_managed_features():
     managed_features = {}
     for filename in os.listdir(MANAGED_FEATURES_DIR):
         if filename.endswith('.yaml'):
+            feature_name = filename.removesuffix('.yaml')
             with open(os.path.join(MANAGED_FEATURES_DIR, filename), 'r') as f:
-                managed_features.update(yaml.safe_load(f))
+                managed_features[feature_name] = yaml.safe_load(f)
+                assert feature_name == managed_features[feature_name]['name']
     return managed_features
 
+
 def create_feature(feature_info):
-    pass
+    with open(os.path.join(FEATURES_DIR, feature_info['name'] + '.json'), 'w') as f:
+        f.write("\n")
+
+
+def empty_feature_dir():
+    for filename in os.listdir(FEATURES_DIR):
+        if filename.endswith('.json'):
+            os.remove(os.path.join(FEATURES_DIR, filename))
+
+
+def main():
+    empty_feature_dir()
+    managed_features = load_managed_features()
+    for feature_info in managed_features.values():
+        create_feature(feature_info)
+
+
+if __name__ == '__main__':
+    main()
